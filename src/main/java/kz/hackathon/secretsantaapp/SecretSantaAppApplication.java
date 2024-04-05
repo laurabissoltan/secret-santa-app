a52eab85e40328c439278ab37ba1af0d7b1e3c5a
laura <laura.bissoltan@nu.edu.kz>
2024-04-05 19:34:47 +0500

    commented out security scheme for the controllers (temporary)

--- src/main/java/kz/hackathon/secretsantaapp/SecretSantaAppApplication.java
+++ src/main/java/kz/hackathon/secretsantaapp/SecretSantaAppApplication.java
@@ -7,14 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SecurityScheme(
+/*@SecurityScheme(
 		name = "bearerAuth",
 		description = "JWT auth description",
 		scheme = "bearer",
 		type = SecuritySchemeType.HTTP,
 		bearerFormat = "JWT",
 		in = SecuritySchemeIn.HEADER
-)
+)*/
 public class SecretSantaAppApplication {
 
 	public static void main(String[] args) {
