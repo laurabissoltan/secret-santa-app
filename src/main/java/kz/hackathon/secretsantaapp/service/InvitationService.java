2118782c213622db228822d6b5fb3f807141d4b5
laura <laura.bissoltan@nu.edu.kz>
2024-04-17 09:56:22 +0500

    link change

--- src/main/java/kz/hackathon/secretsantaapp/service/InvitationService.java
+++ src/main/java/kz/hackathon/secretsantaapp/service/InvitationService.java
@@ -65,7 +65,7 @@ public class InvitationService {
             invitationCode = invitation.getInvitationCode();
         }
         https://secret-santa-app.azurewebsites.net/
-        return "https://secret-santa-app.azurewebsites.net/invitations/accept/" + invitationCode;
+        return "http://51.107.14.25:3000/invitations/accept/" + invitationCode;
     }
 
     public String generateShareableLinkIOS(UUID gameId) {
